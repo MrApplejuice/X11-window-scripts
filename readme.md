eb1ff66cd1bfd19e8781097fa4b595b1be5b6157
Paul Konstantin Gerke <paulkgerke@craftware.info>
2019-06-12 00:42:54 +0200

    Added feature: storing of bound keysequences that can be sent to shortcutted windows

--- readme.md
+++ readme.md
@@ -12,3 +12,36 @@ Templates for integrating custom commands into thunar.
 Name:     Generate a place holder image
 Command:  xfce4-terminal --color-bg="#FF7F7F" --color-text="#000000" --hide-menubar --hide-toolbar --hide-scrollbar --drop-down --title "Generate Placeholder" --working-directory=%f -e "bash /home/paul/myscripts/X11-window-scripts/dl-img-placeholder.sh"
 ~~~~~~~~~~~~~~
+
+## Shortcut window
+
+Required installed software:
+
+- xdotool
+- notify-send
+- zenity
+
+Allows binding keys to windows to activate a given window and send arbitrary
+keyboard inputs to a shortcutted window. The window-manager need to be used
+in to create keybindings for invoking the script.
+
+Usage:
+
+`shortcut-window.sh store [key]`
+
+Stores the currently active window under name `[key]`.
+
+`shortcut-window.sh activate [key]`
+
+Activates the windows stored under name `[key]`.
+
+`shortcut-window.sh rec_key [key] [shortcut-key]`
+
+Creates a keysequence to be sent to window bound to name `[key]`. The name of 
+the stored keysequence will be `[shortcut-key]`.
+
+`shortcut-window.sh send_key [shortcut-key]`
+
+Sends the named keysequence `[shortcut-key]` to the window that was associated
+with the keysequence using `rec_key`.
+
--- shortcut-window.sh
+++ shortcut-window.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+  #!/bin/bash
 
 # This tool allows a user to create a shortcut to quickly activate a certain
 # window in X11. Window key shortcuts are registered globally, therefore, one
@@ -17,14 +17,29 @@
 # Activates the window stored under the given name. Does nothing if the window 
 # cannot be found)
 
-# TODO
-# - Implement heuristic title-based search if now window can be found with a
-#   given id.
 
 config_file="$HOME/.config/shortcut-win.data"
 if [ ! -e "$config_file" ] ; then
     touch "$config_file"
 fi
+
+function clear_name() {
+    tr -dc '[a-zA-Z0-9]' <<< "$1"
+}
+
+function find_window() {
+    name=$( clear_name "$1" )
+    if ! line=$( grep "^$name " $config_file ) ; then
+         notify-send --urgency=low -i error "No window stored using name '$name'"
+         exit 1
+    fi
+    winid=$( awk '{ print $2 }' <<< "$line" )
+    if ! xdotool getwindowname $winid > /dev/null ; then
+         notify-send --urgency=low -i error "No window stored using name '$name'"
+         exit 1
+    fi
+    echo $winid
+}
 
 if [ "$1" == "store" ] ; then
     if [ -z "$2" ] ; then
@@ -37,7 +52,7 @@ if [ "$1" == "store" ] ; then
          exit 1
     fi
     
-    name=$( tr -dc '[a-zA-Z0-9]' <<< "$2" )
+    name=$( clear_name "$2" )
     grep -v "^$name " $config_file > $config_file.2
     mv $config_file.2 $config_file
     echo "$name $winid" >> $config_file
@@ -48,16 +63,50 @@ if [ "$1" == "store" ] ; then
 fi
 
 if [ "$1" == "activate" ] ; then
-    name=$( tr -dc '[a-zA-Z0-9]' <<< "$2" )
-    if ! line=$( grep "^$name " $config_file ) ; then
-         notify-send --urgency=low -i error "No window stored using name '$name'"
+    if ! winid=$(find_window "$2") ; then
+        exit 1
+    fi
+    xdotool windowactivate $winid
+    exit 0
+fi
+
+if [ "$1" == "rec_key" ] ; then
+    win_key_name=$( clear_name "$2" )
+    if ! winid=$(find_window "$win_key_name") ; then
+        exit 1
+    fi
+    window_name="$(xdotool getwindowname $winid)"
+    
+    name=$( clear_name "$3" )
+    if [ -z "$name" ] ; then
+         notify-send --urgency=low -i error "No keyid provided for rec_key command (3rd argument)"
          exit 1
     fi
-    winid=$( awk '{ print $2 }' <<< "$line" )
-    if ! xdotool windowactivate $winid ; then
-         notify-send --urgency=low -i error "No window stored using name '$name'"
+    if ! key="$(zenity --entry --text='Key combination to send to window on '"'$win_key_name'"' for keyid '"'$name'")" ; then
+        exit 0
+    fi
+    key=$( tr -d '[:blank:]' <<< "$key" )
+    
+    grep -v "^!rec_key $name " $config_file > $config_file.2
+    mv $config_file.2 $config_file
+    echo "!rec_key $name $win_key_name $key" >> $config_file
+    
+    notify-send --urgency=low -i info "Registered sending key(s) '$key' to window '$window_name' under keyid '$name'"
+    exit 0
+fi
+
+if [ "$1" == "send_key" ] ; then
+    name=$( clear_name "$2" )
+    if ! line=$(grep "\\!rec_key $name " $config_file) ; then
+         notify-send --urgency=low -i error "No key bound for keyid '$name'"
          exit 1
     fi
+    read _ _ win_key_name key <<< "$line"
+    if ! winid=$( find_window "$win_key_name" ) ; then
+        exit 1
+    fi
+    
+    xdotool key --clearmodifiers --delay 100 --window $winid $( sed 's/,/ /g' <<< "$key" )
     exit 0
 fi
 
